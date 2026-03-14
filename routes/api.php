f140b7b727c5dc064247fd2831fb7927ab322625
Muhammad Farhan <muhfarhanhidayatulloh@gmail.com>
2026-03-15 02:29:30 +0700

    update application timezone to UTC and add RoleSeeder for role management

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::prefix('/auth')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});
+
+Route::post('/test', function () {
+    return response()->json([
+        'message' => 'API WORKING'
+    ]);
+});
